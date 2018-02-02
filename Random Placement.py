5dcefe7340c36698da9334429566e73c4f440323
ivnax <ialexis.cruz@gmail.com>
2018-02-02 08:48:23 -0600

    Update Random Placement.py
    found where the bug seems to be located, but haven't fixed it yet

--- Random Placement.py
+++ Random Placement.py
@@ -2,21 +2,29 @@ import random
 
 circles = []
 
+
 def makeCircle(r):
-    x = random.random()*(38.75-2*r)+r
-    y = random.random()*(38.75-2*r)+r
-    print("checking... \nx: ",x,"\ny: ",y,"\nr: ",r)
-    for each in circles:
-        if not inside(each,x,y,r):
-            circles.append([x,y,r])
+  x = 5 #random.random()*(38.75-2*r)+r
+  y = 10 #random.random()*(38.75-2*r)+r
+  print("checking... \nx: ",x,"\ny: ",y,"\nr: ",r)
+  for each in circles:
+    #this is statement is never the case, even when inside() is hardcoded to return a true
+    if not inside(each,x,y,r): #reason why there is no data to file
+      return circles.append([x,y,r])
+    
+  return circles.append([x,y,r]) ##Bypassing the condition outputs to file fine, the problem is in the inside fucntion
 
-def inside(c,x,y,r):
+     
+
+def inside(c,x,y,r): #intersection fucntion
     intersect = False
-    dist = ( (circles[c][0] - x)**2 + (circles[c][1] - y)**2 )**0.5
-    if dist < circles[c][2] + r:
+    #Bug: The'c' inside of the circles[c][0] is a list and not an integer, which caused the error
+    #Solution: put circles.index(c) instead of c to use and integer value
+    dist = ( (circles[circles.index(c)][0] - x)**2 + (circles[circles.index(c)][1] - y)**2 )**0.5
+    if dist < circles[circles.index(c)][2] + r:        
         intersect = True
-    return intersect
-
+    return False
+    
 def saveToFile():
     data = ""
     for circle in circles:
@@ -27,12 +35,13 @@ def saveToFile():
             else:
                 data += ","
     print("Data:\n"+data)
-    with open("/Users/jacobkrol/Desktop/My Circle Data.csv",'w') as doc:
+    with open("s.csv",'w') as doc:
         doc.write(data)
 
 def main():
     for n in range(5):
         makeCircle(1)
+    circles.append([1,2,"ptasetse"])
     print(circles)
     saveToFile()
 main()
